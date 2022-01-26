2118638bfec0ecfa16c7bcf358296cdb0aec8090
Anjan Dhungana <anjan2052@gmail.com>
2022-01-27 01:22:36 +0545

    added task status (true/false), added completed tasks function

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ const urgencyChild = document.querySelectorAll(".urgency-child");
 const taskArea = document.querySelector(".task-area");
 const userDropdown = document.getElementById("select-users");
 const taskSummary = document.querySelector(".task-summary");
+const checkbox = document.querySelector("i.checkbox");
 
 let addClasses = "";
 
 let userTasks = [];
+let completedTasks = [];
 
 let id = 1;
+let taskid = 1;
 let saturation = 20;
 
 const labels = [
@@ -58,14 +61,22 @@ const viewLabels = () => {
 
 const viewTaskSummary = () => {
   let taskCount = userTasks.length;
-
+  let completedTaskCount = completedTasks.length;
+  let percentageCount = (completedTaskCount / taskCount) * 100;
   if (taskCount == 0) {
     taskSummary.innerHTML = "<h2>No Tasks</h2>";
   } else {
-    taskSummary.innerHTML = `<h1>X%</h1>
+    if (completedTaskCount > 0) {
+      taskSummary.innerHTML = `<h1>${Math.round(percentageCount)}%</h1>
+      <h4 class="text-center">tasks completed</h4>
+      <h5>total tasks: ${taskCount}</h5>
+      <h5>completed: ${completedTaskCount}</h5>`;
+    } else {
+      taskSummary.innerHTML = `<h1>NO</h1>
     <h4 class="text-center">tasks completed</h4>
     <h5>total tasks: ${taskCount}</h5>
-    <h5>completed: y</h5>`;
+    <h5>completed: ${completedTaskCount}</h5>`;
+    }
   }
 };
 
@@ -85,36 +96,63 @@ const clickLabel = (id) => {
   });
 };
 
-const clickAssignBtn = () => {
-  const activeBlobColor = document.querySelector(
-    ".urgency-child.active .urgency-blob"
-  ).style.backgroundColor;
-  userTasks.push({
-    taskName: newTask.value,
-    taskDesc: newDesc.value,
-    taskUser: selectUsers.value,
-    activeBlobColor: activeBlobColor,
-  });
-  viewTaskSummary();
-
+const viewTasks = () => {
   taskArea.innerHTML = userTasks.map(
-    (element) => `<div class="task-detail-card container card w-50" draggable="true">
+    (
+      element
+    ) => `<div class="task-detail-card container card my-3" style="width:25%" draggable="true">
   <div class="card-body">
     <h4 class="card-title text-uppercase">${element.taskName}</h4>
     <p class="subtitle font-weight-light">${element.taskDesc}</p>
     <span>Assigned to: ${element.taskUser} | Priority: High</span>
   </div>
   <div class='task-detail-card-color' style='background-color:${element.activeBlobColor}'></div>
-  <i class='far fa-square checkbox unchecked'></i>
+  <i class='far fa-square checkbox unchecked' onclick="toggleCheckbox(${element.taskid})"></i>
 </div>`
   );
+};
+
+let taskStatus = false;
+
+const clickAssignBtn = () => {
+  const activeBlobColor = document.querySelector(
+    ".urgency-child.active .urgency-blob"
+  ).style.backgroundColor;
+  userTasks.push({
+    taskid: taskid,
+    taskName: newTask.value,
+    taskDesc: newDesc.value,
+    taskUser: selectUsers.value,
+    activeBlobColor: activeBlobColor,
+    taskStatus: taskStatus,
+  });
+  // console.log(userTasks);
+  taskid++;
+  viewTaskSummary();
+
+  viewTasks();
   clearTaskForm();
 };
 
 const clearTaskForm = () => {
   newTask.value = "";
   newDesc.value = "";
-  document.querySelector(".urgency-child.active").forEach((element)=>{
-    element.classList.remove('active');
+  document.querySelector(".urgency-child.active").classList.remove("active");
+};
+
+const toggleCheckbox = (val) => {
+  userTasks.forEach((element) => {
+    if (val == element.taskid) {
+      if(element.taskStatus==false){
+        element.taskStatus = true;
+      }
+      else{
+        element.taskStatus=false;
+      }
+    }
   });
+  completedTasks = userTasks.filter((value) => (value.taskStatus == true));
+  viewTaskSummary();
+
+  console.log(completedTasks);
 };
